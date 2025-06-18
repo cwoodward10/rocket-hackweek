d7175ff5f87ea4351f288988ddcac135a4c80471
Chris Woodward <c.woodward10@gmail.com>
2025-06-18 12:19:46 -0400

    more bloat gone

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -3,17 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 async function bootstrap() {
-  Networker.initialize(UI, UI_CHANNEL);
-
-  UI_CHANNEL.emit(PLUGIN, "hello", ["Hey there, Figma!"]);
-
   const App = (await import("./app")).default;
   
   const rootElement = document.getElementById("root") as HTMLElement;
   const root = ReactDOM.createRoot(rootElement);
   
   onmessage = (e: MessageEvent) => ListenToFigma(e);
-  
+
   root.render(
     <React.StrictMode>
       <App />
